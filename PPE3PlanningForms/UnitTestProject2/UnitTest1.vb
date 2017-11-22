d0aa29f69d99495c4750062c3d3992c59744b0b6
marcguebel <33662671+marcguebel@users.noreply.github.com>
2017-11-22 16:22:49 +0100

    Dbt test

--- PPE3PlanningForms/UnitTestProject2/UnitTest1.vb
+++ PPE3PlanningForms/UnitTestProject2/UnitTest1.vb
@@ -5,7 +5,13 @@ Namespace UnitTestProject2
     Public Class UnitTest1
         <TestMethod>
         Sub TestSub()
-
+            [TestMethod]
+            Public void TestAdditionner()
+            {
+                Calculette c = New Calculette();
+                Double r = c.additionner(4, 6);
+                Assert.AreEqual(r, 10, "Incohérence du résultat d'addition");
+            }
         End Sub
     End Class
 End Namespace
