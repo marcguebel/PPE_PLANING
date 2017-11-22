24d72f2eb9b0eb9767ec2028140cdb7ed08c971c
Anthony BRIEL <anthony.briel@outlook.com>
2017-11-22 14:37:48 +0100

    fom2

--- PPE3PlanningForms/PPE3PlanningForms/Form2.Designer.cs
+++ PPE3PlanningForms/PPE3PlanningForms/Form2.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(133, 12);
+            this.dateTimePicker1.Location = new System.Drawing.Point(144, 12);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
             this.dateTimePicker1.TabIndex = 0;
