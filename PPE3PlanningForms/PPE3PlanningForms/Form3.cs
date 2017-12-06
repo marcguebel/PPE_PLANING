d0c857f51bdebefbfb029a4cfdb51803dc1a0f83
marcguebel <33662671+marcguebel@users.noreply.github.com>
2017-12-06 17:19:35 +0100

    Modif form3

--- PPE3PlanningForms/PPE3PlanningForms/Form3.cs
+++ PPE3PlanningForms/PPE3PlanningForms/Form3.cs
@@ -16,5 +16,9 @@ namespace PPE3PlanningForms
         {
             InitializeComponent();
         }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+        }
     }
 }
