ec8a3d7e6c8a6d14c0975e540ed0d7ac7f53355a
Anthony BRIEL <anthony.briel@outlook.com>
2017-12-11 11:43:28 +0100

    4

--- PPE3PlanningForms/PPE3PlanningForms/Program.cs
+++ PPE3PlanningForms/PPE3PlanningForms/Program.cs
@@ -16,7 +16,7 @@ namespace PPE3PlanningForms
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form4());
         }
     }
 }
